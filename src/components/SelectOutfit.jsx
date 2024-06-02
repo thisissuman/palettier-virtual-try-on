1d38d4dc19691e86469530e34b471f669afe0117
thisissuman <43521912+thisissuman@users.noreply.github.com>
2024-06-02 14:41:36 +0530

    built select outfit menu

--- src/components/SelectOutfit.jsx
+++ src/components/SelectOutfit.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import * as React from "react";
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 
-const SelectOutfit = () => {
+export default function SelectOutfit() {
+  const [age, setAge] = React.useState("");
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+
   return (
-    <div className='border-4 p-48'>SelectOutfit</div>
-  )
+    <div className="pl-10 w-1/4">
+      <Box sx={{ minWidth: 140 }}>
+        <FormControl fullWidth>
+          <InputLabel id="demo-simple-select-label">Select an Outfit</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={age}
+            label="Age"
+            onChange={handleChange}
+          >
+            <MenuItem value={10}>Lehenga Set</MenuItem>
+            <MenuItem value={20}>Kurta Set</MenuItem>
+            
+          </Select>
+        </FormControl>
+      </Box>
+    </div>
+  );
 }
-
-export default SelectOutfit
